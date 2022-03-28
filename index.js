181d0790ecc0b3925f2d7ea82916cd33ce4e1815
Ann <1050123tom@gmail.com>
2022-03-28 16:13:38 +0800

    Change variables' name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const api = require("./api");
 const ADDRESS = "0.0.0.0";
-const PORT = 8080;
+const PORT = 4000;
 const app = express();
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
